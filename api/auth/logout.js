2b519de788f0276b211aaafadd478228f77bf016
empathywithout <cody.e.hutto@gmail.com>
2026-06-14 00:58:50 -0400

    Fix logout: redirect to /portfolio and add Expires header

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -1,5 +1,7 @@
 // api/auth/logout.js
 export default function handler(req, res) {
-  res.setHeader('Set-Cookie', 'tcgw_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
-  res.redirect('/portfolio.html');
+  res.setHeader('Set-Cookie', [
+    'tcgw_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
+  ]);
+  res.redirect('/portfolio');
 }
